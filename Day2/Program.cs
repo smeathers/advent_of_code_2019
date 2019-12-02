56dc6102876c6a060c6ed8591c9f33d91f94b391
Edward Smeathers <edward@smeathers.co.uk>
2019-12-02 23:39:32 +0000

    Day2 Part2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -9,49 +9,76 @@ namespace Day2
     {
         static void Main(string[] args)
         {
+
+            for (int noun = 0; noun <= 99; noun++)
+                for (int verb = 0; verb <= 99; verb++)
+                {
+                    string output = ProgramRun(noun.ToString(), verb.ToString());
+                    //Console.WriteLine("Noun: " + noun + " Verb: " + verb + " Output: " + output);
+                    if (output == "19690720")
+                    {
+                        Console.WriteLine("Noun: " + noun + " Verb: " + verb + " Done");
+                        Console.ReadKey();
+                    }
+                }
+
+
+            //Console.WriteLine(ProgramRun("12","2"));
+        }
+
+        static string ProgramRun(string noun, string verb) { 
             var lines = File.ReadAllLines("input.txt");
+
+            string output = "";
 
             foreach(var line in lines)
             {
                 var positions = line.Split(',');
-                
+
                 // overide start values
                 //positions[1] = "12";
                 //positions[2] = "2";
+                positions[1] = noun;
+                positions[2] = verb;
+
 
 
                 for (int instructionPointer = 0; instructionPointer < positions.Length; instructionPointer += 4)
                 {
                     int opcode = int.Parse(positions[instructionPointer]);
-                    int noun = int.Parse(positions[int.Parse(positions[instructionPointer + 1])]);
-                    int verb = int.Parse(positions[int.Parse(positions[instructionPointer + 2])]);
+                    int value1 = int.Parse(positions[int.Parse(positions[instructionPointer + 1])]);
+                    int value2 = int.Parse(positions[int.Parse(positions[instructionPointer + 2])]);
                     int storeAddress = int.Parse(positions[instructionPointer + 3]);
                     switch (opcode)
                     {
                         case 1:
                             //add
-                            positions[storeAddress] = (noun + verb).ToString();
+                            positions[storeAddress] = (value1 + value2).ToString();
                             break;
                         case 2:
                             //multiply
-                            positions[storeAddress] = (noun * verb).ToString();
+                            positions[storeAddress] = (value1 * value2).ToString();
                             break;
                         case 99:
                             //print and halt
-                            Console.WriteLine(positions[0]);
-                            Console.ReadKey();
+                            output = positions[0];
+                            //Console.WriteLine(output);
+                            instructionPointer = positions.Length;
+                            //if (output == "19690720")
+                            //    Console.ReadKey();
                             break;
                         default:
-                            int value = (100 * noun + verb);
-                            if (value == 19690720)
-                                Console.WriteLine();
-                            positions[storeAddress] = (100 * noun + verb).ToString();
+                            //int value = (100 * noun + verb);
+                            //if (value == 19690720)
+                            //    Console.WriteLine();
+                            //positions[storeAddress] = (100 * noun + verb).ToString();
                             break;
                     }
 
                 }
-
+                
             }
+            return output;
         }
     }
 }
