7e8c3079e372404d657382125f9e5533ea922a7d
Edward Smeathers <edward.smeathers@imserv.com>
2019-12-04 09:02:37 +0000

    Day4 Part1

---
+++ Day4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+// input = 347312-805915
+// part1 = 594
+// part2 = 
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int input = 347312, inputStop = 805915;
+            int passwordCount = 0;
+            
+
+            for (; input <= inputStop; input++)
+            {
+                //Console.WriteLine(input);
+                if (DecreaseCheck(input))
+                {
+                    Console.WriteLine("     " + input);
+                    passwordCount++;
+                }
+                //input.ToString().
+            }
+            Console.WriteLine("Password Count: " + passwordCount);
+        }
+
+        static bool DecreaseCheck(int input)
+        {
+            int? previous = null;
+            bool doubleCheck = false;
+
+            foreach (char number in input.ToString().ToCharArray())
+            {
+                if (previous == null)
+                    previous = int.Parse(number.ToString());
+                else 
+                {
+                    if (int.Parse(number.ToString()) >= previous)
+                    {
+                        if (previous == int.Parse(number.ToString()))
+                        {
+                            doubleCheck = true;
+                        }
+                            
+                        previous = int.Parse(number.ToString());
+                    }
+                    else
+                        return false;
+                }
+
+
+            }
+            return doubleCheck;
+
+        }
+    }
+}
