bea61b1688bf71ace88de89d2604f59a3acc7f6b
Edward <edward@smeathers.co.uk>
2019-12-05 00:00:51 +0000

    Day1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 //https://adventofcode.com/2019/day/1
 
@@ -8,7 +9,47 @@ namespace Day1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int FuelCount = 0;
+            var lines = File.ReadLines("input.txt");
+
+            Console.WriteLine(CalculateFuel(2));
+
+            foreach (var line in lines)
+            {
+
+                int RequiredFuel = CalculateFuel(int.Parse(line));
+
+                FuelCount = FuelCount + RequiredFuel;
+
+                bool flag = true;
+
+                //Maths for Fuel for Fuel.
+                int InterFuel = 0;
+
+                while (flag)
+                {
+                    Console.WriteLine("Beep!");
+                    InterFuel = CalculateFuel(RequiredFuel);
+                    if (InterFuel == 0)
+                        flag = false;
+                    else
+                    {
+                        FuelCount = FuelCount + InterFuel;
+                        RequiredFuel = InterFuel;
+                    }
+                }
+            }
+
+            Console.WriteLine("Fuel Needed: " + FuelCount);
+        }
+
+        static int CalculateFuel(int mass)
+        {
+            int ReturnValue;
+            ReturnValue = (mass / 3) - 2;
+            if (ReturnValue < 0)
+                ReturnValue = 0;
+            return ReturnValue;
         }
     }
 }
